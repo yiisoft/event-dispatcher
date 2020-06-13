6ba6f8ad42d5e9a55a99a4dda7390792a06f07bf
terabytesoftw <terabytesoftw@gmail.com>
2020-06-13 18:05:42 -0400

    Add EventDispatcherProvider to composer.json.

---
+++ config/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\EventDispatcher\Provider\EventDispatcherProvider;
+
+return [
+    'yiisoft/event-dispatcher/eventdispatcher' => EventDispatcherProvider::class,
+];
