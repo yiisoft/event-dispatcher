77143d7e7d685e2d244c4a720d2d3ee3b5b66d03
Alexander Makarov <sam@rmcreative.ru>
2020-10-08 02:39:02 +0300

    Fix #7: Implement DeferredDispatcher

---
+++ src/Dispatcher/DeferredDispatcher.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Yiisoft\EventDispatcher\Dispatcher;
+
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+final class DeferredDispatcher implements EventDispatcherInterface
+{
+    private EventDispatcherInterface $dispatcher;
+    private array $events = [];
+
+    public function __construct(EventDispatcherInterface $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
+
+    public function dispatch(object $event)
+    {
+        $this->events[] = $event;
+    }
+
+    public function flush(): void
+    {
+        foreach ($this->events as $event) {
+            $this->dispatcher->dispatch($event);
+        }
+    }
+}
