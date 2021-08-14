8abd47e213b75cf1d4140c5c8cd837cac1b05a2f
Alexander Makarov <samdark@users.noreply.github.com>
2021-08-14 10:47:57 +0000

    Apply fixes from StyleCI

--- tests/common/Provider/ProviderTest.php
+++ tests/common/Provider/ProviderTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\EventDispatcher\Tests\Provider;
 
-use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Yiisoft\EventDispatcher\Provider\ListenerCollection;
 use Yiisoft\EventDispatcher\Provider\Provider;
